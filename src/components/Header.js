9455a295f5cd6fba8121f9e56d109487e3cdee80
Maddie Lord <madeline.lord@4cinsights.com>
2017-07-13 17:34:19 -0700

    adding component directory

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="headerComponent">
+        <p>Hello World.</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
